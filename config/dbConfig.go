9c5aa810d3d11cc5e30fd7ab25f90d52d80d82cb
mustafapaydas <mustafapaydass@gmail.com>
2023-09-15 21:52:26 +0300

    Update dbConfig.go

--- config/dbConfig.go
+++ config/dbConfig.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 func InitDbConfig() {
 	var err error
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Istanbul", os.Getenv("DATABASE_HOST"),
-		os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWOR"), os.Getenv("DATABASE_NAME"), os.Getenv("DATABASE_PORT"))
+		os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"), os.Getenv("DATABASE_PORT"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
